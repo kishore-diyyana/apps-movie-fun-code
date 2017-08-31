31cce8ebcd06fc4e0967432914f6eec1c065d3bb
Pivotal <pivotal@Pivotals-iMac-2.local>
2017-08-31 12:18:20 -0700

    Fix10: Kishore

--- my-ci/sscript/my-deploy.sh
+++ my-ci/sscript/my-deploy.sh
@@ -2,18 +2,19 @@
 
 set -e +x
 
-pushd my-movie-service
+pushd my-movie-service-git
   echo "Packaging JAR"
   ./mvnw clean package -DskipTests
 popd
 
-jar_count=`find my-movie-service/target -type f -name *.jar | wc -l`
+jar_count=`find my-movie-service-git/target -type f -name *.jar | wc -l`
 
 if [ $jar_count -gt 1 ]; then
   echo "More than one jar found, don't know which one to deploy. Exiting"
   exit 1
 fi
 
-find my-movie-service/target -type f -name *.jar -exec cp "{}" package-output/my-movie-service.jar \;
+find my-movie-service-git/target -type f -name *.jar -exec cp "{}" package-output/my-movie-service.jar \;
 
 echo "Done packaging"
+exit 0
